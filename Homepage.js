535f847509d01de071b739f350073ca46afff911
Shemaremy <remyshema20@gmail.com>
2024-02-11 17:26:42 +0200

    commit 007

--- Homepage.js
+++ Homepage.js
@@ -12,6 +12,34 @@ document.addEventListener("DOMContentLoaded", function() {
 
 
 
+// IMPLEMENTING THE LAZY METHOD
+
+document.addEventListener("DOMContentLoaded", function() {
+    let lazyImages = document.querySelectorAll('.back');
+
+    let options = {
+        rootMargin: '0px',
+        threshold: 0.1
+    };
+
+    let observer = new IntersectionObserver(function(entries, observer) {
+        entries.forEach(function(entry) {
+            if (entry.isIntersecting) {
+                let img = entry.target;
+                img.src = img.dataset.src;
+                observer.unobserve(img);
+            }
+        });
+    }, options);
+
+    lazyImages.forEach(function(img) {
+        observer.observe(img);
+    });
+});
+
+
+
+
 
 
 
