d23a5d26a3b27b656ef1a2bc2ab80c5ed08497d1
Shemaremy <remyshema20@gmail.com>
2024-04-11 21:39:10 +0200

    commit Final output (but not yet)

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const sgMail = require('@sendgrid/mail');
 const { body, validationResult } = require('express-validator');
 
 const app = express();
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 1988;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
